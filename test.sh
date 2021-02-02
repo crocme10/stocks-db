80ba15654c99158bbc5ef7c0659037f704bcf58c
crocme10 <matt@area403.org>
2021-02-02 12:46:34 +0100

    [Fixed] echo docker run command in test

--- test.sh
+++ test.sh
@@ -12,5 +12,7 @@ if docker ps --all | grep -q ${DOCKER_NAME}; then
 fi
 # docker build --tag="${DOCKER_REPO}/${PROJECT}" -f docker/Dockerfile .
 # docker push ${DOCKER_REPO}/${PROJECT}
-docker run -d -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=${POSTGRES_DB} -p 5432:5432/tcp --name ${DOCKER_NAME} ${DOCKER_REPO}/${PROJECT}:${VERSION}
+cmd="docker run -d -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=${POSTGRES_DB} -p 5432:5432/tcp --name ${DOCKER_NAME} ${DOCKER_REPO}/${PROJECT}:${VERSION}"
+echo "$cmd"
+eval "$cmd"
 docker logs -f ${DOCKER_NAME}
