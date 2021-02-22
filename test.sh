aed8eaf3d001a5aae4a7b9f5b873c082f47dad81
crocme10 <matt@area403.org>
2021-02-22 22:45:58 +0100

    Up

--- test.sh
+++ test.sh
@@ -10,9 +10,9 @@ if docker ps --all | grep -q ${DOCKER_NAME}; then
   docker stop ${DOCKER_NAME}
   docker rm ${DOCKER_NAME}
 fi
-# docker build --tag="${DOCKER_REPO}/${PROJECT}" -f docker/Dockerfile .
-# docker push ${DOCKER_REPO}/${PROJECT}
-cmd="docker run -d -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=${POSTGRES_DB} -p 5432:5432/tcp --name ${DOCKER_NAME} ${DOCKER_REPO}/${PROJECT}:${VERSION}"
+docker build --tag="${SNAPSHOT_REPO}/${PROJECT}:${VERSION}" -f docker/Dockerfile .
+docker push ${SNAPSHOT_REPO}/${PROJECT}:${VERSION}
+cmd="docker run -d -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=${POSTGRES_DB} -p 5432:5432/tcp --name ${DOCKER_NAME} ${SNAPSHOT_REPO}/${PROJECT}:${VERSION}"
 echo "$cmd"
 eval "$cmd"
 
